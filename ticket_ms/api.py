ce6bc4b1c1c9ff857b5baaea65d747e993ecc408
shamilnk <shamilnk91@gmail.com>
2023-04-26 09:54:50 +0530

    feature: login api

---
+++ ticket_ms/api.py
@@ -0,0 +1,38 @@
+import frappe
+
+@frappe.whitelist(allow_guest=True)
+def response(message, data, success, status_code):
+    '''method to generates responses of an API
+       args:
+            message : response message string
+            data : json object of the data
+            success : True or False depending on the API response
+            status_code : status of the request'''
+    frappe.clear_messages()
+    frappe.local.response["message"] = message
+    frappe.local.response["data"] = data
+    frappe.local.response["success"] = success
+    frappe.local.response["http_status_code"] = status_code
+    return
+
+@frappe.whitelist(allow_guest=True)
+def login(user, pwd):
+    """
+        metohd to login user and get api key and secret
+        args:
+            user: email of user
+            pwd: password of user
+    """
+    try:
+        login_manager = frappe.auth.LoginManager()
+        login_manager.authenticate(user, pwd)
+        login_manager.post_login()
+    except frappe.exceptions.AuthenticationError:
+        return response( "Authentication Error!", {}, False, 400)
+    # generate api key and secret
+    api_secret = frappe.core.doctype.user.user.generate_keys(frappe.session.user)
+    data = {
+            'api_secret': api_secret['api_secret'],
+            'api_key': frappe.db.get_value('User', frappe.session.user, 'api_key')
+            }
+    return response('Success', data, True, 200)
